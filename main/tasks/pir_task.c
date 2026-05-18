9cea76fd1dd85db1254811d1ace89625bc3fa75f
vishwajit-tiwari <vishwajit16897@gmail.com>
2026-05-18 15:22:13 +0530

    Corrected Interrupt config for PIR sensor

--- main/tasks/pir_task.c
+++ main/tasks/pir_task.c
@@ -49,9 +49,9 @@ void pir_init(void)
     gpio_config_t io_conf = {
         .pin_bit_mask = 1ULL << PIR_GPIO,
         .mode = GPIO_MODE_INPUT,
-        // .pull_up_en = GPIO_PULLUP_DISABLE,
-        // .pull_down_en = GPIO_PULLDOWN_ENABLE,
-        .intr_type = GPIO_INTR_POSEDGE, //!< GPIO interrupt type : rising edge
+        .pull_up_en = GPIO_PULLUP_DISABLE,
+        .pull_down_en = GPIO_PULLDOWN_ENABLE,
+        .intr_type = GPIO_INTR_ANYEDGE,
     };
 
     esp_err_t returnType = gpio_config(&io_conf);
